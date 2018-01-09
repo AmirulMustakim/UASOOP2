786f9bc3847fadc4747ae85b3045d012d69e3f3d
AmirulMustakim <amirulmustakim1997@gmail.com>
2018-01-09 22:42:11 +0700

    versi 2.3.2

---
+++ db/mahasiswa.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 09 Jan 2018 pada 14.29
+-- Versi Server: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(8) NOT NULL,
+  `Nama` varchar(20) NOT NULL,
+  `Jurusan` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `Nama`, `Jurusan`) VALUES
+(1, 'G 1234 MQ', '10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `nim` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15090089;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/resources/static/coba-app.js
@@ -0,0 +1,47 @@
+var cobaApp = angular.module('CobaApp', []);
+
+cobaApp.controller('CobaController', 
+	function($scope) {
+		$scope.daftarNama = [];
+
+		$scope.tambah = function() {
+			$scope.daftarNama.push($scope.nama);
+		}
+	});
+
+cobaApp.controller('ApiController', 
+	    function($scope, $http, $window) {
+	    	$scope.daftarMahasiswa = [];
+
+	    	$scope.updateData = function() {
+	    		$http.get('/daftar-mahasiswa')
+	    			.then(sukses, gagal);
+
+	    		function sukses(response) {
+	    			console.log(response);
+	    			$scope.daftarMahasiswa = response.data;
+	    		};	
+
+	    		function gagal(response) {
+	    			console.log(response);
+	    		};
+	    	};
+
+	    	$scope.ubah = function(mhs) {
+	    		//console.log(mhs);
+	    		$window.location.href = "/edit?nim=" + mhs.nim;
+	    	};
+
+	    	$scope.hapus = function(mhs) {
+	    		$http.delete('/hapus/' + mhs.nim).then(sukses, gagal);
+
+	    		function sukses(response) {
+	    			$scope.updateData();
+	    		};
+
+	    		function gagal(response) {};
+	    	};
+
+	    	$scope.updateData();
+		}
+	);
